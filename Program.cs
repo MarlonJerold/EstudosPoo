55bd7e96bf4cdf13977e788286e6250d34f6d8ea
Marlon Jerold Moura Martins <jeroldmarlon12@gmail.com>
2022-06-09 17:34:54 -0300

    Pimeiro commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExemplosC_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroDeVezes = 5;
+
+            for (int i = 0; i < numeroDeVezes; i++)
+            {
+                System.Console.WriteLine($"Bem-Vindo ao curso de dotnet .Net -> {i}");
+            }
+        }
+    }
+}
